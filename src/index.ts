46b460eae2f94933ef8e617cd0b20c664b489f40
ion-project <gabrielcursino.medeiros@gmail.com>
2021-07-27 19:46:38 -0300

    fix(): Use default type

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { Tooltip } from '@ionited/tooltip';
 import { Directive } from 'vue';
 
 export const tooltip: Directive = {
-  mounted(el: HTMLElement, binding) {
-    (el as any).tooltip = Tooltip(el, binding.value);
+  mounted(el, binding) {
+    el.tooltip = Tooltip(el, binding.value);
   },
 
-  unmounted(el: any) {
+  unmounted(el) {
     el.tooltip.destroy();
   }
 }
